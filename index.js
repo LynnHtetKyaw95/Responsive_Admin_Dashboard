c1448a4fc10388992d70bd8ad2338aa1262f7ffb
Zephyrl <dklin1995@gmail.com>
2023-07-23 14:19:23 +0630

    Switch Light/Dark Theme

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
+// show sidebar
 menuBtn.addEventListener('click', function(){
     sideMenu.style.display = 'block';
 });
 
+// close sidebar
 closeBtn.addEventListener('click', function(){
     sideMenu.style.display = 'none';
 });
+
+// change theme
+themeToggler.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
